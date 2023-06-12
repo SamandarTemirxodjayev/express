4318a33c15e2e4666e0802b7c82a7dd95a3c3887
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-06-12 14:36:46 +0500

    add mongoose

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require('express');
 
 const app = express();
@@ -6,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/crm", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Database connected");
+    app.listen(3000, () => {
+      console.log('Example app listening on port 3000!');
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
